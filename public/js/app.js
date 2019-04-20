b494bebf80ce65b6cc0f8ee471dd26ce14a77c5e
kmietotwy <kmietotwy@gmail.com>
2019-04-19 18:49:39 -1000

    added moment for relative timestamp

--- public/js/app.js
+++ public/js/app.js
@@ -59,35 +59,12 @@ function getData(subreddit) {
       statsDiv.appendChild(dot);
 
       function timeSince(date) {
-        let seconds = Math.floor((new Date() - date) / 1000);
-      
-        let interval = Math.floor(seconds / 31536000);
-      
-        if (interval > 1) {
-          return interval + " years";
-        }
-        interval = Math.floor(seconds / 2592000);
-        if (interval > 1) {
-          return interval + " months";
-        }
-        interval = Math.floor(seconds / 86400);
-        if (interval > 1) {
-          return interval + " days";
-        }
-        interval = Math.floor(seconds / 3600);
-        if (interval > 1) {
-          return interval + " hours";
-        }
-        interval = Math.floor(seconds / 60);
-        if (interval > 1) {
-          return interval + " minutes";
-        }
-        return Math.floor(seconds) + " seconds";
+        return moment(date).fromNow();
       }
 
       let timelapse = document.createElement('div');
       timelapse.className = 'timelapse statsItem';
-      timelapse.innerHTML = timeSince(new Date(post.data.created*1000))
+      timelapse.innerHTML = timeSince(post.data.created*1000)
       statsDiv.appendChild(timelapse);
 
       let dot2 = document.createElement('div');
